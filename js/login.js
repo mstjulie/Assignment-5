88620ccafd154efdb113cd8fec94e356482f6dcd
mstjulie <mstjulie612@gamil.com>
2026-03-07 09:40:01 +0600

    I create login from

---
+++ js/login.js
@@ -0,0 +1,19 @@
+document.getElementById('login-btn').addEventListener('click', function(){
+    const inputUsername = document.getElementById('input-username');
+    const username = inputUsername.value;
+    console.log(username);
+
+    const inputPassword = document.getElementById('input-password');
+    const password = inputPassword.value;
+    console.log(password);
+
+    if(username === "admin" && password === "admin1234"){
+        alert('login Success');
+        window.location.assign("./home.html")
+    }
+    else{
+        alert("login Failed");
+        return;
+    }
+
+})
